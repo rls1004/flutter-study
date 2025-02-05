314777bfb38e3c2854f2a3879d3f3a1c617ce4ef
rls1004 <98nba@naver.com>
2025-02-05 18:12:56 +0900

    remove unused import

--- threads_clone/lib/screens/home_screen.dart
+++ threads_clone/lib/screens/home_screen.dart
@@ -1,8 +1,5 @@
-import 'package:faker/faker.dart' as develop;
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:threads_clone/constants/gaps.dart';
 import 'package:threads_clone/constants/sizes.dart';
 import 'package:threads_clone/screens/widgets/post_view_widget.dart';
 
